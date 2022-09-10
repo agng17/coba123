6a9d292bf2c556bb19450f3b312ad12dc3274c00
agng17 <113221631+agng17@users.noreply.github.com>
2022-09-10 09:30:29 +0700

    commit pertama

---
+++ main.py
@@ -0,0 +1,27 @@
+# Please paste or enter the Python code, then click Run
+print('Hello World')
+
+angka = 12
+angka_pecahan = 1,5
+tipe_boolean = False
+tipe_list = ["Budi", "Tono", "12"]
+tipe_set = {1,2,3,3,3,4,4,"abc"}
+pertambahan = 100 + 2 + 2
+pengurangan = 100 - 72
+perkalian = "nye" * 3
+pembagian = 9 / 3
+pembagian_habis_bagi = 12 // 5
+modulus = 19 % 3
+
+print(angka)
+print("tipe float", angka_pecahan)
+print(tipe_boolean)
+print(tipe_list)
+print(tipe_list[2])
+print(tipe_set)
+print(pertambahan)
+print(pengurangan)
+print(perkalian)
+print(pembagian)
+print(pembagian_habis_bagi)
+print('modulus 19 % 3 = ', modulus)
